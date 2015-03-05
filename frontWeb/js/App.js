4c725fe7367b27976c2531770bc9dc68f2745da8
wumingyu12 <317808023@qq.com>
2015-03-05 15:44:25 +0800

    应用了ui-rounter简单的实例
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- frontWeb/js/App.js
+++ frontWeb/js/App.js
@@ -1,2 +1,28 @@
-var myApp = angular.module('MyApp', []);//[]里可以注入模块
+var myApp = angular.module('MyApp', ['ui.router']);//[]里可以注入模块
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/state1");
+    $stateProvider
+        .state('state1', {
+            url: "/state1",
+            templateUrl: "tpls/state1.html"
+        })
+        .state('state1.list', {
+            url: "/list",
+            templateUrl: "tpls/state1.list.html",
+            controller: function($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('state2', {
+            url: "/state2",
+            templateUrl: "tpls/state2.html"
+        })
+        .state('state2.list', {
+            url: "/list",
+            templateUrl: "tpls/state2.list.html",
+            controller: function($scope) {
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        });
+});
 
