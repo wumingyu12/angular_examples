e147dd62a8e0d4e3ef91734d0369cb3c36aa54f0
wumingyu12 <317808023@qq.com>
2015-03-11 20:51:45 +0800

    ng-blind-html 不能解析带style的html,用过滤器$sec
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- frontWeb/view/MyExample/likeQq/app.js
+++ frontWeb/view/MyExample/likeQq/app.js
@@ -7,10 +7,15 @@ MyApp.controller('testCtrl', function ($scope) {
 		console.log("ffffffff");
 	};
 });
-
+//过滤器，可以让ng-blind-html可以绑定带style的html
+MyApp.filter('to_trusted', ['$sce', function ($sce) {
+	return function (text) {
+		return $sce.trustAsHtml(text);
+	};
+}]);
 //控制器
-MyApp.controller('ueCtrl', function ($scope) {
-	$scope.msgs="";
+MyApp.controller('ueCtrl', function ($scope , $sce) {
+	$scope.msgs='<p><span style="color: rgb(141, 179, 226);">方芳芳</span><br/></p>';
 
 	$scope.setShow=function(){
 		ue.setShow();//引用了外部的全局变量
@@ -19,6 +24,6 @@ MyApp.controller('ueCtrl', function ($scope) {
 
 	$scope.sendMsg=function(){
 		$scope.msgs=$scope.msgs+'<div>'+ue.getContent()+'</div>';//引用了外部的全局变量
-		console.log("显示编辑界面");
+		console.log(ue.getContent());
 	};
 });
