34a7db22a816024e77c5248dae4a6d5a53b94abc
wumingyu12 <317808023@qq.com>
2015-03-11 11:35:07 +0800

    用ng-blind-html来实现信息发送后显示在聊天框内，自动解析html代码
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- frontWeb/view/MyExample/likeQq/app.js
+++ frontWeb/view/MyExample/likeQq/app.js
@@ -1,4 +1,4 @@
-var MyApp=angular.module('myapp', []);
+var MyApp=angular.module('myapp', ['ngSanitize']);//ngSanitize指令ng-blind-htmld
 //控制器
 MyApp.controller('testCtrl', function ($scope) {
 	$scope.test = "你是谁";
@@ -10,8 +10,15 @@ MyApp.controller('testCtrl', function ($scope) {
 
 //控制器
 MyApp.controller('ueCtrl', function ($scope) {
+	$scope.msg="<h1>w我是谁</h1>";
+
 	$scope.setShow=function(){
-		ue.setShow();
+		ue.setShow();//引用了外部的全局变量
+		console.log("显示编辑界面");
+	};
+
+	$scope.sendMsg=function(){
+		$scope.msg=ue.getContent();//引用了外部的全局变量
 		console.log("显示编辑界面");
 	};
 });
