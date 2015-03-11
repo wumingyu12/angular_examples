089a7383e9eed0ffd29b4e7bd844f5f4ceda3a9f
wumingyu12 <317808023@qq.com>
2015-03-11 16:10:22 +0800

    加了联系发送的功能，仿qq聊天室
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- frontWeb/view/MyExample/likeQq/app.js
+++ frontWeb/view/MyExample/likeQq/app.js
@@ -10,7 +10,7 @@ MyApp.controller('testCtrl', function ($scope) {
 
 //控制器
 MyApp.controller('ueCtrl', function ($scope) {
-	$scope.msg="<h1>w我是谁</h1>";
+	$scope.msgs="";
 
 	$scope.setShow=function(){
 		ue.setShow();//引用了外部的全局变量
@@ -18,7 +18,7 @@ MyApp.controller('ueCtrl', function ($scope) {
 	};
 
 	$scope.sendMsg=function(){
-		$scope.msg=ue.getContent();//引用了外部的全局变量
+		$scope.msgs=$scope.msgs+'<div>'+ue.getContent()+'</div>';//引用了外部的全局变量
 		console.log("显示编辑界面");
 	};
 });
