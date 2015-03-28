72f0aea8eaa7639b3c3b199791e474fa4ae70695
wumingyu12 <317808023@qq.com>
2015-03-28 19:06:51 +0800

    go里面带map的结构体转不了为json
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- goService/handlers/onlineUser/onlineUser_main.go
+++ goService/handlers/onlineUser/onlineUser_main.go
@@ -12,31 +12,61 @@ import (
 
 //================json=================
 
-type ColorGroup struct { //字段名要大写要不会给json解释到
-	ID     int
-	Name   string
-	Colors []string
+//type ColorGroup struct { //字段名要大写要不会给json解释到
+//	ID     int
+//	Name   string
+//	Colors []string
+//}
+
+//var group *ColorGroup = &ColorGroup{
+//	ID:     1,
+//	Name:   "Reds",
+//	Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+//}
+
+//一个聊天室结构体
+type ChatRoom struct {
+	IfOnline    int                 //是否是在线聊天室
+	OnlineUsers map[int]*OnlineUser //在线用户的map用OnlineUsers[1]指代
 }
 
-var group *ColorGroup = &ColorGroup{
-	ID:     1,
-	Name:   "Reds",
-	Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+//在线用户的结构体
+type OnlineUser struct {
+	Name string
+	Id   int
+}
+
+var chatRoom *ChatRoom = &ChatRoom{
+	//在线
+	IfOnline:    1,
+	OnlineUsers: make(map[int]*OnlineUser),
 }
 
 //================restful在线用户==========================
 //=========================================================
 //restful服务，在线用户列表,通过id
 func GetOnlineUserById(w http.ResponseWriter, r *http.Request) {
+	//虚拟的用户
+	user1 := &OnlineUser{
+		Name: "伍明煜",
+		Id:   1,
+	}
+	user2 := &OnlineUser{
+		Name: "伍明",
+		Id:   2,
+	}
+	chatRoom.OnlineUsers[1] = user1
+	chatRoom.OnlineUsers[2] = user2
+
 	vars := mux.Vars(r) //r为*http.Request
 	userId := vars["id"]
 	fmt.Println(userId)
 	//fmt.Fprintf(w, userId) //向浏览器发送json或者字符串，这里是变量
 
 	//json
-	b, err := json.Marshal(group)
+	b, err := json.Marshal(chatRoom.OnlineUsers[1])
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("onlineUser_main.go 69行 error:", err)
 	}
 	fmt.Fprintf(w, string(b)) //必须要string,确保没发送其他了否则解释不了为json在angular
 	fmt.Println(string(b))
