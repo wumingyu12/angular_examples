a68c66afbc5af764acf6a380f8a3192bcde48ab2
wumingyu12 <317808023@qq.com>
2015-03-30 00:36:53 +0800

    修改了uibootstrap modal 没有灰色背景的问题
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- frontWeb/view/MyExample/uiBootstrapExample/Modal/app.js
+++ frontWeb/view/MyExample/uiBootstrapExample/Modal/app.js
@@ -4,14 +4,36 @@ MyApp.controller('ModalDemoCtrl',[
 	'$scope',
 	'$modal',//注入modal
 	function ($scope,$modal){
+		$scope.items = ['item1', 'item2', 'item3'];
 		$scope.open=function(size){
 			var modalInstance = $modal.open({
       			templateUrl: 'myModalContent.html',
-      			//controller: 'ModalInstanceCtrl',
+      			controller: 'ModalInstanceCtrl',
       			size: size,
       			backdrop:"static",//点击空白处不会退出modal
-      			keyboard :false //按键盘esc不会退出modal
+      			//keyboard :false, //按键盘esc不会退出modal
+      			resolve: {
+        			items: function () {
+          				return $scope.items;
+        			}
+      			}
     		});
     	} 
 	}
 ]);
+
+MyApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
