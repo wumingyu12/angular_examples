6bbe00312641b5a9d8d7218ff113edc4803583f6
wumingyu12 <317808023@qq.com>
2015-03-31 09:26:46 +0800

    添加了uibootstrap的图片轮播示范
    Signed-off-by: wumingyu12 <317808023@qq.com>

---
+++ frontWeb/view/MyExample/uiBootstrapExample/Carousel/app.js
@@ -0,0 +1,19 @@
+var MyApp=angular.module('myapp', ['ui.bootstrap']);
+//控制器
+MyApp.controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    //var newWidth = 600 + slides.length + 1;
+    var newWidth =slides.length + 1;
+    slides.push({
+      //image: 'http://placekitten.com/' + newWidth + '/300',
+      image:'touxiang/cool-male-avatars-0'+newWidth+'.png',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  };
+});
