70264cd0a4bf330004f81ee48c031e5729d64453
wumingyu12 <317808023@qq.com>
2015-05-02 21:56:20 +0800

    给圆环状态加阴影，怎样调用watch监视指令的变化
    Signed-off-by: wumingyu12 <317808023@qq.com>

--- frontWeb/view/MyExample/someAngularExample/ringState/app.js
+++ frontWeb/view/MyExample/someAngularExample/ringState/app.js
@@ -1,7 +1,7 @@
 var MyApp=angular.module('myapp', []);
 //控制器
 /*
-圆环效果
+CSS3圆环倒计时效果 百度
 http://www.yangqq.com/web/css3demo/index.html
 1用"border-radius"实现圆形
 2用"clip:rect"遮罩为半圆
@@ -21,12 +21,15 @@ MyApp.directive('ringstate',function(){
 			percent:'=percent',
 		},
 		template:
-			//为了半圆裁剪，加position:relative,底圆，这里的颜色将会显示剩下的percent比
-			'<div style="height: 100%;width:100%;background-color:rgb(283,23,0);border-radius:50%;;position:relative;">'+  
+			//为了半圆裁剪，加position:relative,底圆，这里的颜色将会percent比,颜色由init函数根据百分比确定
+			'<div style="height: 100%;width:100%;'+
+						'border-radius:50%;position:relative;'+
+						'box-shadow: 0px 0px 20px 0px black;'+//底圆加10px模糊的阴影，水平偏移，垂直偏移，阴影模糊，阴影突出，颜色
+						'">'+  
 				//用来遮挡旋转进入左边的半圆部分
 				'<div style="height:100%;width:100%;position:absolute;clip:rect(0px,200px,200px,100px);">'+
-					//可旋转的右半圆,这里的颜色将会是显示percent值
-					'<div id="rightcir" style="height: 100%;width:100%;background-color:rgb(28,23,111);'+
+					//可旋转的右半圆,这里的颜色将会是显示剩下percent值
+					'<div id="rightcir" style="height: 100%;width:100%;background-color:rgb(255,255,255);'+
 								'border-radius:50%;'+
 								'position:absolute;top:0;left:0;clip:rect(0px,200px,200px,100px);'+//遮挡左边
 								'transform:rotate({{rightCirDeg}});'+//半圆裁剪，一定要position:absolute或fixed	
@@ -38,8 +41,8 @@ MyApp.directive('ringstate',function(){
 				'</div>'+
 				//用来遮挡旋转进入右边的半圆部分（左半圆部分）
 				'<div style="height:100%;width:100%;position:absolute;clip:rect(0px,100px,200px,0px);">'+
-					//可旋转的左半圆
-					'<div id="leftcir" style="height: 100%;width:100%;background-color:rgb(28,23,111);'+
+					//可旋转的左半圆，颜色显示剩下percent
+					'<div id="leftcir" style="height: 100%;width:100%;background-color:rgb(255,255,255);'+
 								'border-radius:50%;'+
 								'position:absolute;top:0;left:0;clip:rect(0px,100px,200px,0px);'+//遮挡左边
 								'transform:rotate({{leftCirDeg}});'+//半圆裁剪，一定要position:absolute或fixed	
@@ -50,13 +53,15 @@ MyApp.directive('ringstate',function(){
 					'</div>'+
 				'</div>'+
 				//覆盖在正中心的圆，形成圆环，颜色是中间部分的颜色
-				'<div style="height:80%;width:80%;background-color:rgb(280,230,111);'+
+				'<div style="height:80%;width:80%;background-color:rgb(255,255,255);'+
 							'top:10%;left:10%;position:absolute;'+//计算与80%有关
 							'border-radius:50%;'+
+							'box-shadow: 0px 0px 10px 0px black inset;'+//内圆加10px模糊的内阴影
 							'">'+
 				'</div>'+
 			'</div>',
 		link:function(scope,elem,attrs,ctrl){
+			var background=elem.find("div").eq(0);//背景底圆，也是占百分比的颜色条
 			var leftcir=elem.find("div").eq(4);//左半圆
 			var rightcir=elem.find("div").eq(2);//右半圆
 			
@@ -73,13 +78,21 @@ MyApp.directive('ringstate',function(){
 					alert("参数不符合范围");
 					console.log("参数不符合范围,指令中的percent")
 				};
+				//根据百分比确定颜色,从百分0到百分百，绿色到红色渐变
+				var percentcolred=(percentInt/100*255).toFixed(0);//百分比越多越红
+				var percentcolgreed=255-percentcolred;//
+				console.log(background[0]);
+				background.css('background-color','rgb('+percentcolred+','+percentcolgreed+',0)');
+				//background.css('background-color','rgb('+percentcolred+','+percentcolgreed+',0)');
 			}();//在最后加个括号可以让其马上运行,这样可以不加scope.apply()更新template
+			function test(){console.log(111111)}
+			//scope.$watch('percent',test);
 			//=====初始化结束=======================
 		}
 	};
 });
 
 MyApp.controller('test',function($scope){
-	$scope.percent="80";
+	$scope.percent="30";
 });
 
